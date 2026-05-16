a783de1c833062b953161cbc1b73139f2c2ddfe0
admin <admin@LAPTOP-KJ3LD66D>
2026-05-16 09:30:28 +0700

    Add project files.

---
+++ CMS.Data/Entities/Category.cs
@@ -0,0 +1,25 @@
+﻿/*
+Họ và tên: Trần Văn Khánh
+MSSV: 2123210003
+Ngày tạo: 16/05/2026
+version 1
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } // Tên danh mục (vd: Tin Giáo Dục)
+        public string Description { get; set; }
+
+        // Quan hệ: Một danh mục có nhiều bài viết
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+
+}
---
+++ CMS.Data/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿/*
+Họ và tên: Trần Văn Khánh
+MSSV: 2123210003
+Ngày tạo: 16/05/2026
+version 1
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Data.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string Role { get; set; } // Quản trị viên hoặc Biên tập viên
+    }
+}
+
